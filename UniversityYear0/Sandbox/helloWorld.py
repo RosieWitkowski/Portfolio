fdb023a4891e68cbe807b9e7fca0a1385c643884
RosieWitkowski <142828531+RosieWitkowski@users.noreply.github.com>
2025-11-13 23:40:01 +0000

    Delete UniversityYear0 directory

--- UniversityYear0/Sandbox/helloWorld.py
+++
@@ -1,9 +0,0 @@
-# The man, the myth, the legend... hello woorrlllddd
-
-def main():
-    name = input("Name: ").lower
-    print(f"Hello, STM0003!\nHello, World!\nHello, {name}!")
-
-if __name__ == "__main__":
-    main()
-
